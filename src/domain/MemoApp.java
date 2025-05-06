105a9eb8d42dc4e7f9231eaa68f85da5ccebe54c
지유 <jiyu16413@gmail.com>
2025-05-06 22:01:34 +0900

    진행중

--- src/domain/MemoApp.java
+++ src/domain/MemoApp.java
@@ -19,7 +19,10 @@ public class MemoApp extends MemoService {
 	}
 
 	private static final Path MEMO_PATH = Path.of("storage", "Memo", "memo.txt");
-	private List<MemoService> memos = new ArrayList<>();
+	//여기 밑에 싱글턴 만들기
+	private static final MemoApp memoApp = new MemoApp();
+	
+	private List<Memo> memos = new ArrayList<>();
 
 	public MemoApp() {
 
@@ -58,8 +61,53 @@ public class MemoApp extends MemoService {
 //	
 	}
 
+	private static MemoService memoService = new MemoService();
+
+//		private MemoService() {
+//		
+//		}
+	public static MemoService getInstance() {
+		return memoService;
+	}
+
+//		//추가	
+	private void add() {
+		String title = TrioUtils.nextLine("제목: ");
+		String content = TrioUtils.nextLine("내용: ");
+		memos.add(new Memo(title, content));
+		save();
+		System.out.println("메모 추가 및 저장 완료");
+	}
+
+	// 수정
+	private void modify() {
+		if (memos.isEmpty()) {
+			System.out.println("수정할 메모기록이 없습니다");
+		}
+		showMemo();
+		int index = TrioUtils.nextInt("수정할 메모장을 입력하세요");
+		
+		if(index < 0 || index >= memos.size()) {
+			System.out.println("잘못된 입력입니다");
+			return;
+		}
+		
+		Memo mo = memos.get(index);
+		String title;
+	}
+
+	// 삭제
+	private void delete() {
+		if(memos.isEmpty()) {
+			System.out.println("삭제할 메모가 없습니다");
+			return;
+		}
+		showMemo();
+		int index = TrioUtils.nextInt("삭제할 메모장 번호를 입력하세요: ");
+	}
+
 	// 작성 기록- 메모 리스트
-	public void history() {
+	private void history() {
 		System.out.println("작성한 메모");
 		try {
 			if (!Files.exists(MEMO_PATH)) {
@@ -76,71 +124,47 @@ public class MemoApp extends MemoService {
 			System.out.println("작성한 메모를 불러오지 못했습니다:" + e.getMessage());
 		}
 	}
-
-	private static MemoService memoService = new MemoService();
-
-//		private MemoService() {
-//		
-//		}
-	public static MemoService getInstance() {
-		return memoService;
-	}
-
-//		//추가	
-	public void add() {
-		String title;
-	}
-
-	// 메모 제목
-	public void title() {
-
-	}
-
-	// 메모 내용
-	public void content() {
-
-	}
-
-	// 수정
-	public void modify() {
-
-	}
-
-	// 삭제
-	public void delete() {
-
-	}
-
-	// 저장
-	public void save() {
-		List<String> save = new ArrayList<>();
-		for (MemoService m : memos) {
-			save.add(m.toString());
+    //메모 보여주기
+	private void showMemo() {
+		if (memos.isEmpty()) {
+			System.out.println("메모가 비어있습니다");
+			return;
+		}
+		for (int i = 0; i < memos.size(); i++) {
+			Memo m = memos.get(i);
+			System.out.printf("%d: %s\n", i, m.title, m.content);
 		}
 	}
 
-	private String title;
-	private String content;
-
-	public String getTitle() {
-		return title;
+	// 저장
+	private void save() {
+		List<String> save = new ArrayList<>();
+		for (Memo m : memos) {
+			save.add(m.toString());
+		}
+		try {
+			Files.createDirectories(MEMO_PATH.getParent());
+			Files.write(MEMO_PATH, save);
+			System.out.println("메모가 저장되었습니다: " + MEMO_PATH.toAbsolutePath());
+		} catch (IOException e) {
+			System.out.println("메모장을 불러올 수 없습니다:" + e.getMessage());
+		}
 	}
+		private String updateField(String field, String current) {
+			String input = TrioUtils.nextLine("새로운"+field + "(" +current + "): ");
+			return input.isEmpty() ? current : input;
+		}
+	
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+	private static class Memo {
+		String title;
+		String content;
 
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	@Override
-	public String toString() {
-		return "MemoApp [title=" + title + ", content=" + content + "]";
+		Memo(String title, String content) {
+			super();
+			this.title = title;
+			this.content = content;
+		}
 
 	}
 }
