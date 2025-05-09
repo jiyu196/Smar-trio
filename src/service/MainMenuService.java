7f122b80b3dde0cc213392b9e83d03555523cd27
DanGitUser <imtaehoonkim@gmail.com>
2025-05-09 16:28:13 +0900

    MainMenuService 수정

--- src/service/MainMenuService.java
+++ src/service/MainMenuService.java
@@ -21,7 +21,7 @@ public class MainMenuService { // 이 클래스를 호출하는 것으로 메인
 
 	public void mainMenu() {
 		while (true) {
-			int no = nextInt("실행할 기능 선택: ");
+			int no = nextInt("실행할 기능 선택: \n1. 설정 \n2. \n3. \n4.");
 			switch (no) {
 			case 1: {
 				System.out.println("설정 기능");
--- src/service/apps/LottoAppService.java
+++ src/service/apps/LottoAppService.java
@@ -6,26 +6,22 @@ import utils.DateTimeUtils;
 
 public class LottoAppService {
 
-	// 25/04/30
-	public static void main(String[] args) {
+	public void run() {
 		String currentTime = DateTimeUtils.getCurrentDateTime("dd/MM/yyyy hh:mm a");
 		System.out.println(currentTime);
-		
+
 		int[] lotto = new int[6];
 		int idx = 0;
 		while (true) {
 			int number = (int) (Math.random() * 45) + 1;
-
 			boolean insert = true; // 중복 체크. insert
-
+			
 			for (int i = 0; i <= idx; i++) { // 배열 내에 들어있는 값이 중복인지 아닌지
 				if (lotto[i] == number) {
 					insert = false;
 					break;
 				}
-
 			}
-
 			if (insert) {
 				lotto[idx] = number;
 				idx++;
