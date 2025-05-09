042e49cebb0d1ecd0977b4541de639869036c020
KYS <90492881+luminous77777@users.noreply.github.com>
2025-05-09 18:24:49 +0900

    조상 클래스 App 구현

---
+++ src/app/App.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public abstract class App implements Serializable {
+	private final int appNo;
+	private final String appName;
+	private final boolean systemApp;
+
+	public App(int appNo, String appName) {
+		this(appNo, appName, false);
+	}
+
+	public App(int appNo, String appName, boolean systemApp) {
+		super();
+		this.appNo = appNo;
+		this.appName = appName;
+		this.systemApp = systemApp;
+	}
+
+	public int getAppNo() {
+		return appNo;
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public boolean isSystemApp() {
+		return systemApp;
+	}
+	
+	
+}
