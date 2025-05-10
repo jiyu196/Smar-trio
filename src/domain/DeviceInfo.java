447a39bf82ac84a3fa2337f6e8b79a674092030f
luminous77777 <tiger2ys98@gmail.com>
2025-05-11 02:29:53 +0900

    deviceInfo 필드 getter setter 생성

--- src/domain/DeviceInfo.java
+++ src/domain/DeviceInfo.java
@@ -1,9 +1,57 @@
 package domain;
 
-public class DeviceInfo {
+import java.io.Serializable;
+
+public class DeviceInfo implements Serializable{
 	
-	String deviceName = "SmarTrio-0001";
-	String userName;
-	String deivcePw;
-	String userEmail; 
+	private static final String DEVICENAME = "SmarTrio-0001";
+	private String userName;
+	private String deivcePw;
+	private String userEmail;
+	
+	
+	public DeviceInfo(String userName, String userEmail) {
+		this.userName = userName;
+		this.userEmail = userEmail;
+	}
+	
+	public DeviceInfo(String userName, String deivcePw, String userEmail) {
+		super();
+		this.userName = userName;
+		this.deivcePw = deivcePw;
+		this.userEmail = userEmail;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getDeivcePw() {
+		return deivcePw;
+	}
+
+	public void setDeivcePw(String deivcePw) {
+		this.deivcePw = deivcePw;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public static String getDevicename() {
+		return DEVICENAME;
+	}
+
+
+	
+	
+	
 }
