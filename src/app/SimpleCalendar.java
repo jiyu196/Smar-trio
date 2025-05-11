9d3f8e7c8541b346619171cef8203a6bede47b09
DanGitUser <imtaehoonkim@gmail.com>
2025-05-11 10:32:05 +0900

    달력 기능 확장 완료

--- src/app/SimpleCalendar.java
+++ src/app/SimpleCalendar.java
@@ -3,19 +3,43 @@ package app;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import util.TrioUtils;
+
 public class SimpleCalendar extends App{
 
     public SimpleCalendar(int no) {
 		super(no, "달력");
 		// TODO Auto-generated constructor stub
 	}
+    
+    Calendar cal = Calendar.getInstance();
 
 	public void run() {
-        
+        while (true) {
+        	System.out.println("옵션을 선택하세요:");
+        	int choice = TrioUtils.nextInt(" 1: 이전 달\n 2: 다음 달 \n 0: 종료");
+        	switch (choice) {
+			case 1: {
+				cal.add(Calendar.MONTH, -1); // 이전 달
+			    displayCalendar();
+			    break;
+			}
+			case 2: {
+				cal.add(Calendar.MONTH, 1); // 다음 달
+				displayCalendar();
+				break;
+			}
+			case 0: {
+				return; // 종료 + 매인으로
+			}
+			default:
+				System.out.println("잘못된 선택입니다.");
+				break;
+			}
+        }
     }
 	
 	private void displayCalendar() {
-		Calendar cal = Calendar.getInstance();
         // 마지막 날자 (월당)
 		int lastDate = cal.getActualMaximum(Calendar.DATE);
         // 시작 날자
