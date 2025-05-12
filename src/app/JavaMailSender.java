c093d53e365c191eeb9717efb8250667ab885860
KYS <90492881+luminous77777@users.noreply.github.com>
2025-05-12 18:05:28 +0900

    모듈 제거. 이메일 전송 클래스 추가

---
+++ src/app/JavaMailSender.java
@@ -0,0 +1,46 @@
+package app;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class JavaMailSender {
+	public static void main(String[] args) throws AddressException, MessagingException {
+		String id = "tiger2ys98@gmail.com";
+		String pw = "httyqnfhvnpepmvi";
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(id, pw);
+			}
+		});
+		System.out.println(session);
+		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(id));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("tiger2ys98@gmail.com"));
+		
+		message.setSubject("자바 메일 발송 테스트");
+		message.setText("java mail 1.6기반 내용");
+		Transport.send(message);
+		
+		System.out.println("done.");
+		
+	}
+}
